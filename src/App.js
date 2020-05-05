508fa0f97cb5d590876b006175e3c6bd90e9e20b
anmolsukki <anmolsukki94@gmail.com>
2020-05-05 14:45:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import First from "./Components/First";
+
+export default function App() {
+    return (
+        <div style={{textAlign: "center"}}>
+            <First />
+        </div>
+    )
+}
---
+++ src/Components/First.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
+
+class First extends React.Component {
+  constructor(){
+    super()
+      this.state = {
+        step: 1,
+        firstName: "",
+        lastName: "",
+        email: "",
+        occupation: "",
+        city: "",
+        bio: ""
+      }
+  }
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render () {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+    switch (step) {
+    case 1:
+    return (
+      <FormUserDetails nextStep={this.nextStep} handleChange={this.handleChange} values={values} />
+    );
+    case 2:
+      return (
+        <FormPersonalDetails nextStep={this.nextStep} prevStep={this.prevStep} handleChange={this.handleChange} values={values} />
+      );
+    case 3:
+      return (
+        <Confirm nextStep={this.nextStep} prevStep={this.prevStep} values={values} />
+      );
+      case 4:
+        return <Success />;
+      default : return ""
+    }
+  }
+}
+
+export default First;
